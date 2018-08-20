b16a51c0ba5267ba3c4fc1a11852d578ccdeae15
daniel-whitfield-spotcap <daniel.whitfield@spotcap.com>
2018-08-20 23:31:56 +0200

    cut out some crap

--- cirplot.r
+++ cirplot.r
@@ -16,6 +16,7 @@ R <- 1 # this will be changed to 2 when I plot repeats (masking n's)
 
 #Assuming %, this is for GC
 legend.max <- 100
+
 #Change colors here
 col <- c("black", "red", "orange", "yellow", "darkgreen", "grey4")
 col2 <- c("black", "green", "yellow")
@@ -43,13 +44,14 @@ r <- 15.5
 H <- 1
 cir.band <- 2
 Max <- 5
+cir.density <- TRUE
 
-if(file=="jpg") { jpeg(paste("Circular GC and Mask Plot.jpg"), width = 9*dpi,height=7*dpi,res=dpi,quality = 100) }
-# if(file=="pdf") { pdf(paste(paste("Circular GC and Mask Plot.pdf"), width = 9,height=7) }
-# if(file=="tiff") { tiff(paste("Circular GC and Mask Plot.tiff"), width = 9*dpi,height=7*dpi,res=dpi) }
-# if(file=="png") { jpeg(paste("Circular GC and Mask Plot.png")sep=""), width = 9*dpi,height=7*dpi,res=dpi,quality = 100) }
+# Fix this
+if(file=="jpg") jpeg(paste("Circular GC and Mask Plot.jpg"), width = 9*dpi,height=7*dpi,res=dpi,quality = 100)
+#if(file=="pdf") pdf(paste(paste("Circular GC and Mask Plot.pdf"), width = 9,height=7)
+#if(file=="tiff") tiff(paste("Circular GC and Mask Plot.tiff"), width = 9*dpi,height=7*dpi,res=dpi)
+#if(file=="png") jpeg(paste("Circular GC and Mask Plot.png")sep=""), width = 9*dpi,height=7*dpi,res=dpi,quality = 100)
 par(xpd=TRUE)
-
 
 # change from "0.525000" to "52", -- YES IT ROUNDS DOWN
 n_dat$V5 <- as.numeric(as.character(sub("0." , "", n_dat$V5)))
@@ -59,13 +61,6 @@ m_dat <- as.numeric(m_dat)/bin_size
 m_dat <- ceiling(100*m_dat)/100
 m_dat <- as.numeric(as.character(sub("0." , "", m_dat)))
 m_dat <- substr(m_dat, 0, 2)
-
-# make the curvature for the circular plot
-circle.plot <- function(myr,type="l",x=NULL,lty=1,lwd=1,col="black",add=TRUE,n.point=1000)
-{
-  curve(sqrt(myr^2-x^2),xlim=c(-myr,myr),n=n.point,ylim=c(-myr,myr),type=type,lty=lty,col=col,lwd=lwd,add=add)
-  curve(-sqrt(myr^2-x^2),xlim=c(-myr,myr),n=n.point,ylim=c(-myr,myr),type=type,lty=lty,col=col,lwd=lwd,add=TRUE)
-}
 
 # make as matrix
 zzz <- as.matrix(n_dat)
@@ -79,10 +74,6 @@ zzz.xy.index <- which(!as.numeric(zzz[, 1]) %in% c(0 : max.chr))
 
 #if(length(zzz.xy.index) != 0){
 chr.xy <- unique(zzz[zzz.xy.index, 1])
-#  for(i in 1:length(chr.xy)){
-#    zzz[zzz[, 1] == chr.xy[i], 1] <- max.chr + i
-#  }
-#}
 
 zzz <- zzz[order(as.numeric(zzz[, 1]), as.numeric(zzz[, 2])), ]
 chr <- zzz[, 1]
@@ -96,7 +87,6 @@ chr.labels <- unique(n_dat$V1)
 Num <- as.numeric(table(n_dat[,1]))
 Nchr <- length(Num)
 N <- NULL
-pvalueT <- as.matrix(n_dat[,3])
 pvalue.pos <- n_dat[, 2]
 pvalue.posN <- NULL
 ticks <- NULL
@@ -109,25 +99,17 @@ if (!missing(band)) {
 }
 if (band==0)  band=1
 
-if (LOG10) {
-  pvalueT[pvalueT <= 0] <- 1
-  pvalueT[pvalueT > 1] <- 1
-}
-
 for (i in 0:(Nchr-1)) {
   if (i==0){
-    #pvalue <- append(pvalue,rep(Inf,band),after=0)
     pvalue.posN <- pvalue.pos.list[[i+1]] + band
     ticks[i+1] <- max(pvalue.posN)-floor(max(pvalue.pos.list[[i+1]])/2)
-  }else{
-    #pvalue <- append(pvalue,rep(Inf,band),after=sum(Num[1:i])+i*band)
+  } else {
     pvalue.posN <- c(pvalue.posN, max(pvalue.posN) + band + pvalue.pos.list[[i+1]])
     ticks[i+1] <- max(pvalue.posN)-floor(max(pvalue.pos.list[[i+1]])/2)
   }
-} 
-pvalue.posN.list <- tapply(pvalue.posN, n_dat[, 1], list)
-TotalN <- max(pvalue.posN)
+}
 
+TotalN <- max(pvalue.posN)
 pos.x <- list()
 col.index <- list()
 col2.index <- list()
@@ -142,11 +124,12 @@ for(i in 1 : length(chr.num)) {
 col <- colorRampPalette(col)(100)
 col2 <- colorRampPalette(col2)(100)
 
-# THIS IS IT
+# THIS IS IT -- Outer plot
 for (i in 1 : length(chr.num)) {
   col.seg <- c(col.seg, col[col.index[[i]]])
 }
 
+# Inner Plot
 for (i in 1 : length(chr.num)) {
   col2.seg <- c(col2.seg, col2[col2.index[[i]]])
 }
@@ -159,16 +142,6 @@ if (length(zzz.xy.index) != 0) {
 
 chr.num <- rev(chr.num)
 
-if (chorm.maxlen > 10000000) {
-  top_axis_length <- 10
-  kilo_or_mega <- 1e6    # mega
-  text_size <- "Mb"
-} else {
-  top_axis_length <- 5
-  kilo_or_mega <- 1e3  # kilo
-  text_size <- "Kb"
-}
-
 # Make legend not so large, yet informative
 legend.y <- seq(10, 90, 5)
 legend2.y <- seq(10,90,5)
@@ -179,14 +152,6 @@ legend2.y.col <- legend2.y
 legend.col <- col[seq(10,90,5)]
 legend2.col <- col2[seq(10,90,5)]
 
-if (length(chr.den.col) > 1) {
-  cir.density=TRUE
-  den.fold <- 20
-  #density.list <- list(den.col=col.seg, legend.col=legend.col, legend.y=legend.y)
-  
-} else {
-  cir.density <- FALSE
-}
 if (!file.output) {
   if (!is.null(dev.list())) dev.new(width=8, height=8)
   par(pty="s", xpd=TRUE, mar=c(1,1,1,1))
@@ -200,52 +165,45 @@ if (cir.density) {
   plot(NULL,xlim=c(1.05*(-RR-4*cir.chr.h),1.05*(RR+4*cir.chr.h)),ylim=c(1.05*(-RR-4*cir.chr.h),1.05*(RR+4*cir.chr.h)),axes=FALSE,xlab="",ylab="")
 }
 
+segments(
+  (1.3 * RR)*sin(2*pi*(pvalue.posN-round(band/2))/TotalN),
+  (1.3 * RR)*cos(2*pi*(pvalue.posN-round(band/2))/TotalN),
+  (1.3 * RR+cir.chr.h)*sin(2*pi*(pvalue.posN-round(band/2))/TotalN),
+  (1.3 * RR+cir.chr.h)*cos(2*pi*(pvalue.posN-round(band/2))/TotalN),
+  col=col.seg, lwd=0.8
+)
+legend(
+  75,
+  1,
+  title="GC %", legend=legend.y, pch=15, pt.cex = 1, col = legend.col,
+  cex=0.5, bty="n",
+  y.intersp=1,
+  x.intersp=1,
+  yjust=0.5, xjust=0, xpd=TRUE
+)
 
-if (cir.density) {
-  segments(
-    (1.3 * RR)*sin(2*pi*(pvalue.posN-round(band/2))/TotalN),
-    (1.3 * RR)*cos(2*pi*(pvalue.posN-round(band/2))/TotalN),
-    (1.3 * RR+cir.chr.h)*sin(2*pi*(pvalue.posN-round(band/2))/TotalN),
-    (1.3 * RR+cir.chr.h)*cos(2*pi*(pvalue.posN-round(band/2))/TotalN),
-    col=col.seg, lwd=0.8
-  )
-  legend(
-    75,
-    1,
-    title="GC %", legend=legend.y, pch=15, pt.cex = 1, col = legend.col,
-    cex=0.5, bty="n",
-    y.intersp=1,
-    x.intersp=1,
-    yjust=0.5, xjust=0, xpd=TRUE
-  )
-}
+#ticks1=1.4*(RR+cir.chr.h)*sin(2*pi*(ticks-round(band/2))/TotalN)
+#ticks2=1.4*(RR+cir.chr.h)*cos(2*pi*(ticks-round(band/2))/TotalN)
 
-# ticks1=1.4*(RR+cir.chr.h)*sin(2*pi*(ticks-round(band/2))/TotalN)
-# ticks2=1.4*(RR+cir.chr.h)*cos(2*pi*(ticks-round(band/2))/TotalN)
+#for(i in 1:length(ticks)){
+  #angle=360*(1-(ticks-round(band/2))[i]/TotalN)
+  #text(ticks1[i],ticks2[i],chr.labels[i],srt=angle,font=2, cex=0.6)
+#}
 
-# for(i in 1:length(ticks)){
-  # angle=360*(1-(ticks-round(band/2))[i]/TotalN)
-  # text(ticks1[i],ticks2[i],chr.labels[i],srt=angle,font=2, cex=0.6)
-# }
-
-
-if (cir.density) {
-  segments(
-    (8*cir.band+RI)*sin(2*pi*(pvalue.posN-round(band/2))/TotalN),
-    (8*cir.band+RI)*cos(2*pi*(pvalue.posN-round(band/2))/TotalN),
-    (8*cir.band+RI+cir.chr.h)*sin(2*pi*(pvalue.posN-round(band/2))/TotalN),
-    (8*cir.band+RI+cir.chr.h)*cos(2*pi*(pvalue.posN-round(band/2))/TotalN),
-    col=col2.seg, lwd=0.8
-  )
-  legend(
-    86,
-    1,
-    title="Mask %", legend=legend2.y, pch=15, pt.cex = 1, col = legend2.col,
-    cex=0.5, bty="n",
-    y.intersp=1,
-    x.intersp=1,
-    yjust=0.5, xjust=0, xpd=TRUE
-  )
-}
-
+segments(
+  (8*cir.band+RI)*sin(2*pi*(pvalue.posN-round(band/2))/TotalN),
+  (8*cir.band+RI)*cos(2*pi*(pvalue.posN-round(band/2))/TotalN),
+  (8*cir.band+RI+cir.chr.h)*sin(2*pi*(pvalue.posN-round(band/2))/TotalN),
+  (8*cir.band+RI+cir.chr.h)*cos(2*pi*(pvalue.posN-round(band/2))/TotalN),
+  col=col2.seg, lwd=0.8
+)
+legend(
+  86,
+  1,
+  title="Mask %", legend=legend2.y, pch=15, pt.cex = 1, col = legend2.col,
+  cex=0.5, bty="n",
+  y.intersp=1,
+  x.intersp=1,
+  yjust=0.5, xjust=0, xpd=TRUE
+)
 dev.off()
